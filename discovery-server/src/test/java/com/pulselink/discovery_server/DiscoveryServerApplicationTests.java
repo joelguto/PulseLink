25c43700e9e54d27d09d2ae5a140ce127dcf250d
Joel Guto <joelguto@Joels-MacBook-Air.local>
2025-07-25 14:55:29 +0300

    Initial commit for the discovery server setup

---
+++ discovery-server/src/test/java/com/pulselink/discovery_server/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pulselink.discovery_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
