75977cf3138d62b8842461fcfd8b3843fb21568b
Joel Guto <joelguto715@gmail.com>
2025-07-28 23:57:15 +0300

    implemented the auth server for the pulse link service

---
+++ auth-server/src/main/java/com/pulselink/auth_server/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.pulselink.auth_server.repository;
+
+import com.pulselink.auth_server.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
---
+++ user-service/src/main/java/com/pulselink/user_service/dto/UserInfoDTO.java
@@ -0,0 +1,8 @@
+package com.pulselink.user_service.dto;
+
+public class UserInfoDTO {
+     private long id;
+     private String name;
+     private String email;
+     private String phone;
+}
